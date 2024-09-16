8a833ab3dfce2b5d26b3c1330166ac7b754a6e99
Luiza Goes <luizagoes087@gmail.com>
2024-09-16 17:17:17 +0000

    Aula modificada

--- Aula5J.java
+++ Aula5J.java
@@ -8,6 +8,8 @@ public class Aula5J{
         Random aleatorio = new Random();
         Scanner leitura = new Scanner(System.in);
         final int sorteio = aleatorio.nextInt(10);
+        var teste = sorteio;
+        System.out.println("O número do sorteio é: " + teste);
         
         // Obtendo valores com o usuário
         System.out.println("Digite um numero: ");
@@ -26,16 +28,27 @@ public class Aula5J{
             System.out.println("Errou, você está com " + pontuacao + " pontos");
             System.out.println("Digite novamente: ");
             numero = leitura.nextInt();
+
+            if(numero == sorteio) {
+                System.out.println("Parabéns, você acertou com " + pontuacao + " pontos");
+                leitura.close();
+                return;
+            } else {
             pontuacao = pontuacao - 10;
             System.out.println("Errou, você está com " + pontuacao + " pontos");
             System.out.println("Digite novamente: ");
             numero = leitura.nextInt();
+            }
+            if(numero == sorteio) {
+                System.out.println("Parabéns, você acertou com " + pontuacao + " pontos");
+                leitura.close();
+                return;
+            } else{
             pontuacao = pontuacao - 10;
             System.out.println("Perdeu, você está com " + pontuacao + " pontos");
+            }
         
         }
-        
-        
         
     
         leitura.close();
